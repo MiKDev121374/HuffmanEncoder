56e122c9a3caa8757469528d1cd4edae25d38ba7
Shaelyn Watson <shaelyn@cs.utexas.edu>
2013-10-04 16:43:39 -0500

    initial definition of alphabet

---
+++ Encoder.java
@@ -0,0 +1,27 @@
+
+class Alphabet{
+	//Defines alphabet to be used for encoding/decoding
+	char A = 'A';
+	char B = 'B';
+	char C = 'C';
+	char D = 'D';
+	char E = 'E';
+	char F = 'F';
+	char G = 'G';
+
+}
+
+class Encoder{
+	public static void main(String[] args){
+
+		//define alphabet
+		Alphabet alphabet = new Alphabet();
+
+
+		//if (args[0].equals("frequenciesFile")){
+			//then import file and align these frequencies with the alphabet
+		//if (args[1].equals("k")){
+			//then generate a file to encode/decode of length k with given frequency probabilities
+
+	}
+}
